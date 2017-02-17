3eadff11d31f09caef01f80f2d8fd9c5936d9f56
Dawson Graham <hdawsong@gmail.com>
2017-02-16 19:37:10 -0800

    Add initial questions to vampires.rb file

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year = gets.chomp.to_i
+
+puts "Our company cafeteria servs garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
+
