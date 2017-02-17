ca489796df37b3ee7e1234a98b958494412462cd
Dawson Graham <hdawsong@gmail.com>
2017-02-16 20:17:29 -0800

    Add first condition to detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,9 +7,36 @@ age = gets.chomp.to_i
 puts "What year were you born?"
 year = gets.chomp.to_i
 
-puts "Our company cafeteria servs garlic bread. Should we order some for you?"
+match = false
+if age == (2017-year) || age == (2016-year)
+	match = true
+else
+	match = false
+end
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic_bread = gets.chomp
+
+willing = false
+if garlic_bread.downcase == "yes" || garlic_bread.downcase == "ya" ||
+  garlic_bread.downcase == "y" 
+	willing = true
+else
+	willing = false
+end
 
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
 
+signup = false
+if health_insurance.downcase == "yes" || health_insurance.downcase == "ya" || health_insurance.downcase == "y"
+	signup = true
+else
+	signup = false
+end
+
+if match == true && (willing == true || signup == true)
+	puts "Probably not a vampire."
+else
+	puts "Results inconclusive."
+end
