de71d17cbf4deb8275b5d463f91ed40988a09665
Dawson Graham <hdawsong@gmail.com>
2017-02-17 17:01:40 -0800

    Add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -12,5 +12,20 @@ p encrypt_word
 end
 
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+def decrypt (word)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+	length = word.length
+	decrypt_word = ""
+	index = 0
+	a = 0 
+	while index < length
+		encrypt_letter = word[a]
+		decrypt_letter = alphabet.index("#{encrypt_letter}")
+		decrypt_letter = decrypt_letter - 1 
+		decrypt_word += (alphabet[decrypt_letter])
+		index += 1
+		a +=1 
+	end
+	p decrypt_word
+end
 
