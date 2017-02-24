7b64a3a0c32d3a351417a2c2762b33ec59c4274e
Dawson Graham <hdawsong@gmail.com>
2017-02-24 15:05:42 -0800

    Add nested data structure of GSW roster

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,34 @@
+warriors = {
+	'Point Guards' => [
+		'Stephen Curry',
+		'Shaun Livingston',
+		'Patrick McCaw',
+		'Briante Weber'
+	],
+	'Shooting Guards' => [
+		'Klay Thompson',
+		'Ian Clark'
+	],
+	'Small Forwards' => [
+		'Kevin Durant',
+		'Andre Iguodala',
+		'Kevon Looney',
+		'James Michael McAdoo'
+	],
+	'Power Forwards' => [
+		'Draymond Green',
+		'David West'
+	],
+	'Centers' => [
+		'Zaza Pachulia',
+		'JaVale McGee',
+		'Damian Jones'
+	]
+}
+
+p warriors['Point Guards'][1]
+
+p warriors['Centers'][2]
+
+p "Here's the Golden State Warriors' Small Forwards: #{warriors['Small Forwards']}"
+
