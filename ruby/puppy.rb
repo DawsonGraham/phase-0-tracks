2f490ddf0c6176870c4c4cbe31b70ccd8808c50f
Dawson Graham <hdawsong@gmail.com>
2017-02-28 14:49:12 -0800

    Define puppy class for 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES -------------------------
+# German Shepherd
+
+# CHARACTERISTICS -----------------
+# Fur length: medium
+# Eye count: 2
+# Friendly: yes
+# Name: Ralph
+
+# BEHAVIOR --------------------
+# Energetic
+# Playful
+# Protective
