df9f767225e0ae93d8db972fe6f0dcec519f6ee5
Dawson Graham <hdawsong@gmail.com>
2017-03-02 14:54:42 -0800

    Add random santa generator with sample ethnicity, gender, hometown and age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,7 @@
 class Santa
 	attr_reader :ethnicity, :hometown
+	attr_accessor :gender, :age
+
 	def initialize(gender, ethnicity, hometown)
 		print "Initializing Santa instance..."
 		@gender = gender 
@@ -10,7 +12,7 @@ class Santa
 	end
 
 	def speak
-		puts "Ho, ho, ho! Haaaappy holidays! I'm a #{@ethnicity} #{@gender}, from #{@hometown}.\r\n"
+		puts "Ho, ho, ho! Haaaappy holidays! I'm a #{@ethnicity} #{@gender} Santa from #{@hometown}.\r\n"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
@@ -25,10 +27,6 @@ class Santa
 		@reindeer_ranking.delete(reinder_name)
 		@reindeer_ranking.insert(8, reinder_name)
 		puts "#{@reindeer_ranking}"
-	end
-
-	def gender=(new_gender)
-		@gender = new_gender
 	end
 
 end
@@ -55,13 +53,21 @@ end
 
 
 p santa.celebrate_birthday
-
+p santa
 santa.get_mad_at("Vixen")
-
+p santa 
 p santa.celebrate_birthday
 
 p santa 
-
 santa.gender=("Male")
-
 p santa
+
+100.times do
+	santa = Santa.new(example_genders.sample, example_ethnicities.sample, example_hometowns.sample)
+	santa.speak
+	santa.age = rand(140)
+	puts "Age: #{santa.age}"
+end
+
+
+
