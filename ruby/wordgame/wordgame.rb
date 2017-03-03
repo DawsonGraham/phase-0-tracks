71838e86675b920f758b024609944cb49f09b761
Dawson Graham <hdawsong@gmail.com>
2017-03-03 14:53:24 -0800

    Finalize user interface and correct array filter logic for user input

--- ruby/wordgame/wordgame.rb
+++ ruby/wordgame/wordgame.rb
@@ -1,11 +1,12 @@
 class WordGame
-  attr_reader :guess_count, :correct_word, :secret_word
+  attr_reader :guess_count, :correct_word, :secret_word, :blank_array
   
   def initialize
     @guess_count = 0 
     @correct_word = false 
     @secret_word = ""
     @counter = 0 
+    @blank_array = []
   end
   
   def user_secret_word(x)
@@ -17,7 +18,6 @@ class WordGame
   
   
   def guess_word(x)
-     blank_array = []
     if x == @secret_word
       puts "Congrats! You guessed '#{@secret_word}' which was the secret word!"
       @correct_word = true 
@@ -25,15 +25,16 @@ class WordGame
       x.length == 1 
       word_array = @secret_word.split("")
       until @counter == 0  
-        blank_array << "_"
+        @blank_array << "_"
         @counter = @counter - 1 
       end
       if word_array.include?(x)
         index = word_array.index(x)
-        blank_array.insert(index, x)
-        p blank_array
+        @blank_array.delete_at(index)
+        @blank_array.insert(index, x)
+        p @blank_array
       else
-        p blank_array
+        p @blank_array
       end
     end
     @guess_count = @guess_count - 1 
@@ -44,9 +45,8 @@ end
 # user interface    
 puts "Welcome to the Word Game!"
 game = WordGame.new 
-    
-puts "Enter a word for the other user to guess"
-  word = gets.chomp
+  puts "Enter a word for the user to guess"
+  word = gets.chomp 
   game.user_secret_word(word)
   num_of_guesses = word.length
 
