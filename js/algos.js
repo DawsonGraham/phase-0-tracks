6674b7aafeb033d0dc92f3e5a2619e73efe8f994
Dawson Graham <hdawsong@gmail.com>
2017-03-09 21:03:24 -0800

    Modified

--- js/algos.js
+++ js/algos.js
@@ -14,8 +14,20 @@ function Array(string_array) {
     console.log(longest_word + " is the longest word in the array")
 }
 
+function Match(hash1, hash2) {
+	for(var i = 0; i < hash1.length; i++) {
+		if (hash1(i) == hash2(i))
+			console.log(hash1(i));
+	}
+}
+
 var newarray = new Array (["Red","Blue", "Green", "Magenta"])
 
-var animals = ["Lizard", "Crocodile", "Hippopotamus"]
+var animals = ["Lizard", "Crocodile", "Hippopotamus"];
 
-var testArray = new Array(animals)
+var testArray = new Array(animals);
+
+x = {name: "Dawson", age: 24, hair_color: "black"};
+y = {name: "Jane", age: 24, hair_color: "brown"};
+
+testMatch = new Match(x, y);
